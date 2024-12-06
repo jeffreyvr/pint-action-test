64b2902c3d376f79e42b1c962dd7dafa13a10cf6
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2024-12-06 12:10:37 +0100

    test

---
+++ resources/views/template.php
@@ -0,0 +1,7 @@
+<div>
+
+<?php
+echo \PintActionTest\PintActionTest::exampleStringMethod();
+?>
+
+</div>
---
+++ src/PintActionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PintActionTest;
+
+class PintActionTest
+{
+    //
+
+    public static function exampleStringMethod()
+    {
+        return 'TEST';
+    }
+
+    public static function testNamespace()
+    {
+        return new \stdClass;
+    }
+
+    public static function oldArrayFormat() {
+        return array();
+    }
+}
